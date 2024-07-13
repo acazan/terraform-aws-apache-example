15b2478eb86fcfb06f548017cd8a2c5c2d23d95e
Alex Cazan <acazan@kitashi.acitservices.com>
2024-07-13 12:49:12 -0400

    added instance_id in output.tf to be further used

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "public_ip" {
   value       = aws_instance.my_server.public_ip
   description = "Public IP address of the EC2 instance"
 }
+
+output "instance_id" {
+  value       = aws_instance.my_server.id
+  description = "ID of the EC2 instance"
+}
